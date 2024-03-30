17a6e7432bdd5d7d84cdab4a255843a39aed7dc3
Gustavo Rodriguez <gustavo.liccioni@weareadaptive.com>
2024-03-30 20:15:34 +0100

    adding vite ui

---
+++ r2dbc-h2-demo-ui/vite.config.js
@@ -0,0 +1,8 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/r2dbc-h2-demo"
+})
